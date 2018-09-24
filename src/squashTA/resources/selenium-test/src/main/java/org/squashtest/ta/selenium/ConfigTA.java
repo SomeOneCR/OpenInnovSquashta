6a9e7d006643975777fbeb9e48eeb8a592bb1974
SomeOneCR <rakhimchaimaa@outlook.com>
2018-09-24 17:54:28 +0100

    commenting some lines

--- src/squashTA/resources/selenium-test/src/main/java/org/squashtest/ta/selenium/ConfigTA.java
+++ src/squashTA/resources/selenium-test/src/main/java/org/squashtest/ta/selenium/ConfigTA.java
@@ -51,11 +51,12 @@ public class ConfigTA {
 		
 		//driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver");
 	        //WebDriver driver = new ChromeDriver();
-   		ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.addArguments("--headless");
-                chromeOptions.addArguments("--no-sandbox");
+   		//ChromeOptions chromeOptions = new ChromeOptions();
+                //chromeOptions.addArguments("--headless");
+                //chromeOptions.addArguments("--no-sandbox");
 
-                WebDriver driver = new ChromeDriver(chromeOptions);
+               // WebDriver driver = new ChromeDriver(chromeOptions);
+                WebDriver driver = new ChromeDriver();
 	        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 	        driver.manage().window().maximize();
 	        return driver;
